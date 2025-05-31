856b9d183158d47d3e5886520d50827b78c0871d
FloHofmann <123389257+FloHofmann@users.noreply.github.com>
2025-05-31 21:37:33 +0200

    Update init.lua to include transparent background toggle

--- init.lua
+++ init.lua
@@ -14,3 +14,42 @@ require('mason-lspconfig').setup({
         end,
     },
 })
+
+-- Store current state
+local is_transparent = false
+
+-- Toggle background transparency
+local function toggle_background()
+	if is_transparent then
+		-- Solid background
+		vim.api.nvim_set_hl(0, "Normal", { bg = "#1d2021" }) -- Gruvbox dark0_hard
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1d2021" })
+		is_transparent = false
+		print("Background set to solid")
+	else
+		-- Transparent background
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		is_transparent = true
+	end
+end
+
+-- Command to toggle background
+vim.api.nvim_create_user_command("ToggleBackground", toggle_background, {})
+
+-- Ensure transparency is applied after the colorscheme loads
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		if is_transparent then
+			vim.defer_fn(function()
+				vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+				vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			end, 10)
+		end
+	end,
+})
+
+vim.keymap.set("n", "<leader>tt", toggle_background)
+
+toggle_background()
